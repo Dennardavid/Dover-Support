78d822820a73a7f9e7ba76e5e4033608e0c7371d
David DENNAR <dennardavid@gmail.com>
2025-05-15 15:21:41 +0100

    IT/IS added to list

--- src/components/signupform.tsx
+++ src/components/signupform.tsx
@@ -150,7 +150,7 @@ export default function SignUp() {
           >
             <option value="">Select Discipline</option>
             <option value="Reception">Reception</option>
-            <option value="IT/IS">Reception</option>
+            <option value="IT/IS">IT/IS</option>
             <option value="Mechanical">Mechanical</option>
             <option value="Telecoms">Telecoms</option>
             <option value="Instrumentation">Instrumentation</option>
