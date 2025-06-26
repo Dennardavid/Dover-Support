e491bd459b406c9558e34736bd3f7f5aa6dc1428
David DENNAR <dennardavid@gmail.com>
2025-06-26 09:00:44 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["http://10.234.84.88:8080", "http://localhost:8080"],
+  allowedDevOrigins: ["http://10.234.84.146:8080", "http://localhost:8080"],
 };
 
 export default nextConfig;
